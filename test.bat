25008f849cd6132394c1a74c5aa474dc3c978a17
Ahmet Salih <ahmet.salih@windowslive.com>
2017-07-07 17:47:45 +0300

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,19 +1,20 @@
-START node balancer.js
+START node balancer\balancer.js
+timeout 3
 SET SID=1
-START node server.js
+START node server-example.js
 SET SID=2
-START node server.js
+START node server-example.js
 SET SID=3
-START node server.js
-START node client.js
-START node client.js
-START node client.js
-START node client.js
-START node client.js
-START node client.js
-START node client.js
-START node client.js
-START node client.js
-START node client.js
-START node client.js
-START node client.js
+START node server-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
+START node client-example.js
