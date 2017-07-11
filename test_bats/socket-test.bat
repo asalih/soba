195e5640429be9a7d74ce970cece3df357f58b74
Ahmet Salih <ahmet.salih@windowslive.com>
2017-07-11 17:34:54 +0300

    Add files via upload

--- test_bats/socket-test.bat
+++ test_bats/socket-test.bat
@@ -2,16 +2,16 @@ START node ../app.js
 timeout 3
 SET SID=1
 SET PORT=1667
-START node ../socket/server-example.js
+START node ../socket_example/server-example.js
 SET SID=2
 SET PORT=1668
-START node ../socket/server-example.js
+START node ../socket_example/server-example.js
 SET SID=3
 SET PORT=1669
-START node ../socket/server-example.js
-START node ../socket/client-example.js
-START node ../socket/client-example.js
-START node ../socket/client-example.js
-START node ../socket/client-example.js
-START node ../socket/client-example.js
-START node ../socket/client-example.js
+START node ../socket_example/server-example.js
+START node ../socket_example/client-example.js
+START node ../socket_example/client-example.js
+START node ../socket_example/client-example.js
+START node ../socket_example/client-example.js
+START node ../socket_example/client-example.js
+START node ../socket_example/client-example.js
