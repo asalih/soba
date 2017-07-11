50be8c274e3552b7fba8cde587603c38bc735dd8
Ahmet Salih <ahmet.salih@windowslive.com>
2017-07-11 11:37:54 +0300

    Update balancer.js

--- balancer/balancer.js
+++ balancer/balancer.js
@@ -1,52 +1,31 @@
-﻿var net = require("net");
-
-/**
- * predefined consts
- * client-example.js connects to "clientport"
- * server-example.js connects to "serverPort"
- * inactivity for a client defined with "clientTimeout"
- */
-const serverPort = 1557;
-const clientPort = 1447;
-const clientTimeout = 5000;
+var net = require("net");
 
 var servers = [];
 var request = 0;
-var local = net.createServer(function (socket) {
-    socket.name = socket.remoteAddress + ":" + socket.remotePort;
-    servers.push(socket);
+module.exports = (() => {
+    this.balanceServer = {};
+    var passRequest = function (server, data, client) {
+        var socket = new net.Socket();
+        socket.connect(server.port, server.ip, function () {
+            socket.write(data);
+        });
+        socket.on('data', function (data) {
+            client.write(data.toString());
+        });
+    };
+    this.start = function(options) {
+        servers = options.servers;
+        this.balanceServer = net.createServer(function (client) {
+            client.on('data', function (incoming) {
+                var server = servers[(request++) % servers.length];
+                passRequest(server, incoming, client);
+            });
+            client.setTimeout(options.clientTimeout);
+        });
 
-    socket.on('data', function (data) {
-        var fromServer = JSON.parse(data);
-        clients[fromServer.name].write(fromServer.data.toString());
-    });
+        this.balanceServer.listen(options.port);
+    };
 
-});
-local.listen(serverPort);
+    return this;
+})();
 
-var clients = [];
-var server = net.createServer(function (socket) {
-    socket.name = socket.remoteAddress + ":" + socket.remotePort;
-
-    clients[socket.name] = socket;
-    socket.on('data', function (incoming) {
-
-        var server = servers[(request++) % servers.length];
-        var data = JSON.stringify({ name: socket.name, data: incoming.toString() });
-        server.write(data);
-    });
-    socket.on('close', function () {
-        console.log('Connection closed');
-        delete clients[socket.name];
-    });
-    socket.on("error", function (err) {
-        if (err && err.code != "ECONNRESET")
-            throw err;
-    });
-    socket.on("timeout", function (obj) {
-        delete clients[socket.name];
-    });
-    socket.setTimeout(clientTimeout);
-});
-
-server.listen(clientPort);
