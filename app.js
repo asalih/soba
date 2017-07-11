c692285c32485b8ea25ca72fd75cd24ec09a0518
Ahmet Salih <ahmet.salih@windowslive.com>
2017-07-11 17:23:29 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 ﻿var balancer = require("./balancer/balancer");
 
-balancer.start({
-    servers: [{ port: 1557, ip: "127.0.0.1" }, { port: 1558, ip: "127.0.0.1" }, { port: 1559, ip: "127.0.0.1" }],
+//http ports
+balancer().start({
+    servers: [{ port: 1557, ip: "127.0.0.1" }],
     port: 1447,
     clientTimeout: 5000
-})
+});
+
+//socket ports
+balancer().start({
+    servers: [{ port: 1667, ip: "127.0.0.1" }, { port: 1668, ip: "127.0.0.1" }, { port: 1669, ip: "127.0.0.1" }],
+    port: 1337,
+    clientTimeout: 5000
+});
