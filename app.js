676dc970282dc9fe614aec469e7bb685e8575f84
Ahmet Salih <ahmet.salih@windowslive.com>
2017-07-11 17:26:06 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //http ports
 balancer().start({
-    servers: [{ port: 1557, ip: "127.0.0.1" }],
+    servers: [{ port: 1557, ip: "127.0.0.1" }, { port: 1558, ip: "127.0.0.1" }, { port: 1559, ip: "127.0.0.1" }],
     port: 1447,
     clientTimeout: 5000
 });
